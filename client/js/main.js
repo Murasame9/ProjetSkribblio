ae97eee604803f9c4b2d4a8a5605e84a48a60102
Mura <m.colin@ludus-academie.com>
2020-03-17 18:31:42 +0100

    Changement de couleur et ajout de la palette

--- client/js/main.js
+++ client/js/main.js
@@ -6,7 +6,7 @@ var room = 1;
 
 socket.emit("setPseudo",pseudo); // Donne un pseudo
 socket.emit("setRoom",room); // Donne une room
-socket.emit("affichageMessagesPrecedents"); // affiche les messages envoyé dans le chat avant que l'utilisateur ne se connect
+socket.emit("affichageMessagesPrecedents"); // affiche les messages envoyé dans le chat avant que l'utilisateur ne se connecte
 
 socket.on("log", function(data){
     console.log(data);
@@ -61,6 +61,8 @@ var ctx = canvas.getContext("2d");
 
 var jeDoisDessiner = false;
 
+// Multi debut
+
 function sendCanvasData(){
     var imgData = ctx.getImageData(0,0,100, 100);
     socket.emit("updateCanvas", imgData);
@@ -78,8 +80,43 @@ function update(timestamp) {
     requestAnimationFrame(update);
 }
 
+// Multi fin
+
+// Fonctions qui gère la couleur DEBUT
+var couleurs = document.createElement('div');
+        couleurs.className = "couleurs";
+ 
+var palette = document.getElementById("palette");
+var currentColor = "rgb(0,0,0)";
+var Tcolors = ['silver','gray','black','red','darkred','yellow','olive',
+    'lime','green','aqua','teal','blue','navy','fuchsia','purple'];
+    
+var couleurs = document.createElement('div');
+        couleurs.className = "couleurs";
+ 
+    var palette = document.getElementById("palette");
+
+function createPalette(){
+    palette.innerHTML='';
+
+    for(var i = 0; i < 15 ;i++){//Tcolors.length
+        var couleurs = document.createElement('div');
+        couleurs.className = "couleurs";
+        palette.appendChild(couleurs);
+        couleurs.style.background =Tcolors[i];
+        couleurs.addEventListener('click',coloringChoice);
+    }
+    console.log("Palette fini");
+}
+
+function coloringChoice(e){
+    currentColor = e.target.style.backgroundColor;
+    console.log("Changer");
+}      
+// Fonctions qui gère la couleur FIN
+
 function dessiner() {
-    ctx.strokeStyle = "rgb(0,0,0)";
+    ctx.strokeStyle = currentColor;
     ctx.lineWidth = 5;
     ctx.lineTo(mouseX,mouseY);
     ctx.stroke();
@@ -102,3 +139,5 @@ function onDocMouseMove(e) {
     mouseX = e.clientX;
     mouseY = e.clientY;
 }
+
+createPalette();
